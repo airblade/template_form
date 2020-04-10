cf5e8f506d153be6230364a7107432aa886e5a28
Andy Stewart <boss@airbladesoftware.com>
2020-04-10 12:27:08 +0100

    Extract method for clarity

--- lib/template_form.rb
+++ lib/template_form.rb
@@ -4,11 +4,17 @@ require 'template_form/form_helper'
 module TemplateForm
 
   def self.field_error_proc
-    Proc.new { |html_tag, instance| html_tag }
+    noop_field_error_proc
   end
 
   def self.form_type
     :bulma
+  end
+
+  private
+
+  def self.noop_field_error_proc
+    Proc.new { |html_tag, instance| html_tag }
   end
 
 end
