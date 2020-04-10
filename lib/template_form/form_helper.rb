dc2a358cdec82efefcf9f8a851145e50ac238b86
Andy Stewart <boss@airbladesoftware.com>
2020-04-10 12:29:53 +0100

    Use form_with instead of form_for
    See #2.

--- lib/template_form/form_helper.rb
+++ lib/template_form/form_helper.rb
@@ -3,11 +3,11 @@ require 'template_form/form_builder'
 module TemplateForm
   module FormHelper
 
-    def template_form_for(record, options = {}, &block)
+    def template_form_with(model: nil, scope: nil, url: nil, format: nil, **options, &block)
       options[:builder] = TemplateForm::FormBuilder
 
       with_template_form_field_error_proc do
-        form_for record, options, &block
+        form_with model: model, scope: scope, url: url, format: format, **options, &block
       end
     end
 
